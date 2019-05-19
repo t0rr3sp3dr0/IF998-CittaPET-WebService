df22d159a794076a20b4a12fcda9864a03805b50
Pedro Tôrres <t0rr3sp3dr0@gmail.com>
2019-05-19 20:10:52 -0300

    Domingo

--- server.js
+++ server.js
@@ -9,47 +9,58 @@ var assert = require('assert');
 const app = express();
 
 const client = new services.MapClient('localhost:50051', grpc.credentials.createInsecure());
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
 
-const BusEvent = mongoose.model('BusEvent', { unit: String, timestamp: String, coordinate: {easting: String, northing: String }});
+const schema = new mongoose.Schema({
+    unit: String,
+    timestamp: String,
+    coordinate: {
+        easting: String,
+        northing: String,
+    },
+}, {
+    collection: 'bus_event',
+});
+
+const BusEvent = mongoose.model('BusEvent', schema);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("mongoose connected!")
+db.once('open', function () {
+    console.log("mongoose connected!")
 });
 
 app.get('/busesLocation', (_, res) => {
-	res.header("Content-Type", "text/event-stream");
-  res.header("Cache-Control", "no-cache");
-  res.header("Connection", "keep-alive");
- 
-  setInterval(() => {
-		// enviar mapa atualizado
+    res.header("Content-Type", "text/event-stream");
+    res.header("Cache-Control", "no-cache");
+    res.header("Connection", "keep-alive");
 
-	client.getBusesLocation(new messages.GetBusesLocationRequest(), function(err, response) {
-		const busesLocationList = response.toObject().busesLocationList;
-		console.log(busesLocationList);
-			res.write("data: " + JSON.stringify(busesLocationList) + "\n\n");
-	  });
-  }, 100);
+    setInterval(() => {
+        // enviar mapa atualizado
+
+        client.getBusesLocation(new messages.GetBusesLocationRequest(), function (err, response) {
+            const busesLocationList = response.toObject().busesLocationList;
+            console.log(busesLocationList);
+            res.write("data: " + JSON.stringify(busesLocationList) + "\n\n");
+        });
+    }, 1000);
 });
 
 app.get('/unitList', (req, res) => {
-	res.header("Content-Type", "text/event-stream");
-  res.header("Cache-Control", "no-cache");
-  res.header("Connection", "keep-alive");
- 
-	// enviar lista de unidades de onibus
+    res.header("Content-Type", "text/event-stream");
+    res.header("Cache-Control", "no-cache");
+    res.header("Connection", "keep-alive");
 
-	BusEvent.distinct('unit', function (err, result) {
-		if (err) return handleError(err);
-	
-		assert(Array.isArray(result));
-		console.log('unique units', result);
-		res.json(result)
-	})
-	
+    // enviar lista de unidades de onibus
+
+    BusEvent.distinct('unit', function (err, result) {
+        if (err) return handleError(err);
+
+        assert(Array.isArray(result));
+        console.log('unique units', result);
+        res.json(result)
+    })
+
 });
 
 app.use('/', (_, res) => {
